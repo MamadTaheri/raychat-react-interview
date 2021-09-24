2e9238da0fba453571c310824edcc06d19b180c2
Mohammad Taheri <mamad.taheri.68@gmail.com>
2021-09-24 19:30:14 +0330

    edit Topbar

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const Topbar = () => {
-    return (
-        <div className="row mt-2 mt-md-5">
-            <div className="col-md-12">
-               <h4>مکالمات</h4>
-               <p className="text-muted">پیام های خودکار ارسال کنید، ابزارک را در صفحات خاص نمایش دهید و یا در صفحات خاصی پنهان کنید</p>
-               <hr />
-            </div>
-            
+  return (
+      <div className="row mt-2 mt-md-5 p-2 px-md-5">
+        <div className="col-md-12">
+          <h4>مکالمات</h4>
+          <p className="text-muted">
+            پیام های خودکار ارسال کنید، ابزارک را در صفحات خاص نمایش دهید و یا
+            در صفحات خاصی پنهان کنید
+          </p>
+          <br />
+          <hr />
         </div>
-    );
+      </div>
+  );
 };
 
 export default Topbar;
